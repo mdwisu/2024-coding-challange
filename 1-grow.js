2d4ddcf2d5c108a9155d53a2c793c059269f9380
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2024-05-01 22:10:57 +0700

    day 1

---
+++ 1-grow.js
@@ -0,0 +1,18 @@
+// function grow(x) {
+//   let result = x[0];
+//   for (let i = 1; i < x.length; i++) {
+//     result *=  x[i];
+//   }
+//   return result;
+// }
+
+// function grow(x) {
+//   const result = x.reduce((acc, curr) => acc * curr);
+//   return result;
+// }
+
+const grow = (x) => x.reduce((acc, curr) => acc * curr);
+
+console.log(grow([1, 2, 3]));
+console.log(grow([4, 1, 1, 1, 4]));
+console.log(grow([2, 2, 2, 2, 2, 2]));
