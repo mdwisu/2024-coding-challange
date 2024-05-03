af2620bd8392a53c3c4473be12ce7f8bb44d07ab
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2024-05-03 15:18:55 +0700

    day #3

---
+++ 3-hero.js
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/59ca8246d751df55cc00014c
+
+// function hero(bullets, dragons) {
+//   if (bullets / 2 >= dragons) {
+//     return true   
+//   } else {
+//     return false
+//   }
+// }
+
+// function hero(bullets, dragons) {
+//   return bullets / 2 >= dragons
+// }
+
+const hero = (bullets, dragons) => bullets / 2 >= dragons
+
+hero(10, 5); // true
+hero(5, 5); // false
